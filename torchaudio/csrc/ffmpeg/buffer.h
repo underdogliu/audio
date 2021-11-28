74bdda470a11573df743015ceb74a0c130fa1e40
moto <855818+mthrok@users.noreply.github.com>
2021-11-28 15:03:25 -0500

    Add Buffer class

---
+++ torchaudio/csrc/ffmpeg/buffer.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <torch/torch.h>
+#include <torchaudio/csrc/ffmpeg/ffmpeg.h>
+#include <deque>
+
+namespace torchaudio {
+namespace ffmpeg {
+
+class Buffer {
+  std::deque<torch::Tensor> chunks;
+  AVMediaType media_type;
+
+ public:
+  Buffer(AVMediaType type);
+  void push_audio_frame(AVFrame* pFrame);
+  void push_video_frame(AVFrame* pFrame);
+
+  torch::Tensor pop_all();
+};
+
+} // namespace ffmpeg
+} // namespace torchaudio
